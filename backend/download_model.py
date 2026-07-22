6d65ed94b80fa9c0baa4b4a8c7ab62068e279904
Jayanta nath <nathjayanta772@gmail.com>
2026-07-22 14:12:54 +0530

    removed

--- backend/download_model.py
+++
@@ -1,9 +0,0 @@
-# download_model.py
-from transformers import CLIPModel, CLIPProcessor
-
-model_name = "openai/clip-vit-base-patch32"
-
-CLIPModel.from_pretrained(model_name, cache_dir="./models")
-CLIPProcessor.from_pretrained(model_name, cache_dir="./models")
-
-print("Models Downloaded")
